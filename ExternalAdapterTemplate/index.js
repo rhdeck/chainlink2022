e1c1b545e7a2220e6e0a83937ecf88fa68b5d50c
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-05-05 11:44:00 -0400

    EA works with curl -> need to host it on a node

--- ExternalAdapterTemplate/index.js
+++ ExternalAdapterTemplate/index.js
@@ -1,4 +1,5 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
+require('dotenv').config()
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -12,23 +13,35 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
+  exchange: ['exchange'],
+  symbol: ['symbol'],
   endpoint: false
 }
+const APCA_API_KEY_ID = process.env.APCA_API_KEY_ID
+const APCA_API_SECRET_KEY = process.env.APCA_API_SECRET_KEY
+
+const headers = {
+  'APCA-API-KEY-ID': APCA_API_KEY_ID,
+  'APCA-API-SECRET-KEY': APCA_API_SECRET_KEY
+}
+
+console.log(headers)
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
+
+  const exchange = validator.validated.data.exchange
+  const symbol = validator.validated.data.symbol
+  // const endpoint = validator.validated.data.endpoint || 'price'
+  const url = `https://data.alpaca.markets/v1beta1/crypto/${symbol}/quotes/latest?exchange=${exchange}`
+  // const fsym = validator.validated.data.base.toUpperCase()
+  // const tsyms = validator.validated.data.quote.toUpperCase()
 
   const params = {
-    fsym,
-    tsyms
+    exchange,
+    symbol
   }
 
   // This is where you would add method and headers
@@ -36,10 +49,14 @@ const createRequest = (input, callback) => {
   // The default is GET requests
   // method = 'get'
   // headers = 'headers.....'
+  
+  // console.log(headers)
   const config = {
     url,
-    params
+    // params,
+    headers
   }
+  console.log({config})
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
@@ -48,8 +65,12 @@ const createRequest = (input, callback) => {
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
+      // response.data.result = Requester.validateResultNumber(response.data, [tsyms])
+      // console.log(JSON.stringify(response, null, 2))
+      console.log(response.data)
+      console.log({jobRunID: jobRunID})
+      console.log({status: response.status})
+      callback(response.status, Requester.success(jobRunID, { data: {ap:response.data.quote.ap}}))
     })
     .catch(error => {
       callback(500, Requester.errored(jobRunID, error))
