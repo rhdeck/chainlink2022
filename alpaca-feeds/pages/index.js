d8cc9fc5ef8a682a4fe8b20232238423f5ba64d3
Robert Reinhart <robertreinhart24@gmail.com>
2022-05-15 22:53:16 -0400

    CSS changes to algin with Finity

--- alpaca-feeds/pages/index.js
+++ alpaca-feeds/pages/index.js
@@ -1,29 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useEffect, useState, useCallback } from 'react'
-import instance from '../instance'
-import instanceKeeper from '../instanceKeeper'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState, useCallback } from "react";
+import instance from "../instance";
+import instanceKeeper from "../instanceKeeper";
 
 export default function Home() {
-
   const [price, setPrice] = useState([]);
   const [equities, setEquities] = useState([]);
+
+  const loader = (
+    <div className={styles.overlay}>
+      <div className={styles.overlay__inner}>
+        <div className={styles.overlay__content}>
+          <img src="../images/abstract.png" className={styles.spinner}></img>
+        </div>
+      </div>
+    </div>
+  );
 
   const getPriceandFeed = async () => {
     let newPrice = [];
 
     let priceBlock;
     try {
-      for (let i=0; i<equities.length; i++) {
-        priceBlock= await instance.getPriceandBlock(equities[i])
-        newPrice.push(`Price: ${Number(priceBlock[0]/100)}`);
+      for (let i = 0; i < equities.length; i++) {
+        priceBlock = await instance.getPriceandBlock(equities[i]);
+        newPrice.push(`Price: ${Number(priceBlock[0] / 100)}`);
       }
-      setPrice(newPrice)
+      setPrice(newPrice);
     } catch (err) {
-      console.log("Error Message: ", err.message)
+      console.log("Error Message: ", err.message);
     }
-  }
+  };
 
   const getEquities = async () => {
     const equitiesLength = await instanceKeeper.numEquities();
@@ -31,57 +40,60 @@ export default function Home() {
     let allEquities = [];
 
     try {
-      for (let i=0; i<Number(equitiesLength); i++) {
-        equity = await instanceKeeper.equities(i)
-        allEquities.push(equity)
+      for (let i = 0; i < Number(equitiesLength); i++) {
+        equity = await instanceKeeper.equities(i);
+        allEquities.push(equity);
       }
-      setEquities(allEquities)
+      setEquities(allEquities);
     } catch (err) {
-      console.log("Error Message: ", err.message)
+      console.log("Error Message: ", err.message);
     }
-  }
+  };
 
   useEffect(() => {
     getEquities();
-  }, [])
+  }, []);
 
   useEffect(() => {
     getPriceandFeed();
-  }, [equities])
+  }, [equities]);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Alpaca Feeds (Powered by PolyNodes)</title>
-        <meta name="description" content="Current Alpaca feeds running on Polygon Mainnet" />
+        <meta
+          name="description"
+          content="Current Alpaca feeds running on Polygon Mainnet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.logo}>Alpaca Feeds</div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Alpaca Feeds
-        </h1>
-        <div className={styles.gridTwo}>
-        <div className={styles.grid}>
+        <h1 className={styles.title}>Alpaca Feeds</h1>
+        {!price.length > 0 ? (
           <div className={styles.cardTwo}>
-            <h2>Equities</h2>
+            {loader}
           </div>
-
-  
-        </div>
-        <div className={styles.grid}>
-          
-          {price.map((price, index) => {
-            return (
-          <div key={index} className={styles.card}>
-          <h2>{equities[index]}</h2>
-         <p>{price}</p>
-        </div>
-            )
-          })}
-
-        </div>
-        </div>
+        ) : (
+          <div className={styles.gridTwo}>
+            <div className={styles.grid}>
+              <div className={styles.cardTwo}>
+                <h2>Equities</h2>
+              </div>
+            </div>
+            <div className={styles.grid}>
+              {price.map((price, index) => {
+                return (
+                  <div key={index} className={styles.card}>
+                    <h2>{equities[index]}</h2>
+                    <p>{price}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -94,5 +106,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
