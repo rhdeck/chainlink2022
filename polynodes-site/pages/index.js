12a6368dabe98dee2f1d935e94727bfa42c5d44a
Ray Deck <ray@raydeck.com>
2022-05-25 09:04:40 -0400

    feat: home page text and button styling

--- polynodes-site/pages/index.js
+++ polynodes-site/pages/index.js
@@ -3,10 +3,9 @@ import styles from "../styles/Home.module.css";
 import { useEffect, useState, useCallback } from "react";
 import { ethers } from "ethers";
 import { useRouter } from "next/router";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
-
   const router = useRouter();
 
   const clickFooter = useCallback(() => {
@@ -24,21 +23,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.logo}>
-        <div className={styles.logoText}><Link href="/">PolyNodes</Link></div>
+        <div className={styles.logoText}>
+          <Link href="/">PolyNodes</Link>
+        </div>
       </div>
       <main className={styles.main}>
         <h1 className={styles.header1}>PolyNodes</h1>
-        <p className={styles.indexText}>PolyNodes is a platform created to enhance the use of the Polygon 
-        blockchain on Chainlink by making it extremely easy to create Polygon Chainlink nodes. 
-          With the click of a button, users can create and deploy Polygon nodes to run on Chainlink. Users 
-          can then create jobs to run 
-          on those nodes.</p>
-    <button className={styles.finityButton} onClick={() => router.replace('./nodes')}>Start Exploring Nodes</button>
-          
+        <p className={styles.indexText}>
+          The easiest way to make your own Chainlink Oracle on Polygon.
+        </p>
+        <p className={styles.indexText}>
+          With the click of a button, create and deploy Polygon nodes to run on
+          Chainlink. Make your job as easily as defining a javascript lambda.
+        </p>
+        <button
+          className={styles.finityButton}
+          onClick={() => router.replace("./nodes")}
+        >
+          Start Exploring Nodes
+        </button>
       </main>
 
       <footer className={styles.footer}>
-        <button className={styles.finityButton} onClick={() => router.replace("/nodes")}>
+        <button
+          className={styles.finityButton}
+          onClick={() => router.replace("/nodes")}
+        >
           <div style={{ marginRight: "5px" }}>Created with </div>
           <img
             className={styles.finityLogo}
@@ -49,4 +59,3 @@ export default function Home() {
     </div>
   );
 }
-
