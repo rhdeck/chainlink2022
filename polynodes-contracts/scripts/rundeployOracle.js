e0e2cd18d43ff925c8f3652c8b83d41801676f34
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-05-24 21:50:57 -0400

    Added sampleSourceInput for job creation

--- polynodes-contracts/scripts/rundeployOracle.js
+++ polynodes-contracts/scripts/rundeployOracle.js
@@ -4,14 +4,15 @@ const {ethers} = require('ethers');
 
 const {deployMumbai, deployMatic} = require('./deployOracle');
 (async ()=>{
-    // await deployMumbai(process.argv[2],process.argv[3]);
-    const contractAddress = await deployMatic(process.argv[2],process.argv[3]);
-    const provider = new ethers.providers.AlchemyProvider("matic", process.env.ALCHEMY_MATIC_KEY);
-    const account_from = {
-    privateKey: process.env.PK,
-    };
-    let wallet = new ethers.Wallet(account_from.privateKey, provider);
-    const oracle = new ethers.Contract(contractAddress, config.abi, provider);
+    await deployMumbai(process.argv[2],process.argv[3]);
+    await deployMatic(process.argv[2],process.argv[3]);
+    // const contractAddress = await deployMatic(process.argv[2],process.argv[3]);
+    // const provider = new ethers.providers.AlchemyProvider("matic", process.env.ALCHEMY_MATIC_KEY);
+    // const account_from = {
+    // privateKey: process.env.PK,
+    // };
+    // let wallet = new ethers.Wallet(account_from.privateKey, provider);
+    // const oracle = new ethers.Contract(contractAddress, config.abi, provider);
 
     // const txn = await oracle.getChainlinkToken();
     // const receipt_setOwner = await txn.wait();
