99808a4f70c7e7c0cde797fa07efa99110010eab
Robert Reinhart <robertreinhart24@gmail.com>
2022-05-25 16:33:38 -0400

    Try Adding Editor Again

--- polynodes-site/pages/node/[node].js
+++ polynodes-site/pages/node/[node].js
@@ -119,7 +119,7 @@ function Node() {
               <div className={styles.overlay__inner}>
                 <div className={styles.overlay__content}>
                   <img
-                    src="../images/abstract.png"
+                    src="../../images/abstract.png"
                     className={styles.spinner}
                   ></img>
                   <div style={{ textAlign: "center" }}>Retrieving {nodeId} Jobs</div>
